448c855af1d090fc89910f77d0b7d02227455940
hxzhao527 <haoxiangzhao@outlook.com>
2018-12-26 20:27:03 +0800

    fix

--- pkg/proxy/response.go
+++ pkg/proxy/response.go
@@ -9,16 +9,21 @@ import (
 
 var errLogger = log.New(os.Stderr, "", log.LstdFlags)
 
-func ReturnServerError(w http.ResponseWriter, err error) {
-	w.WriteHeader(500)
+func ReturnInternalServerError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
 	msg := fmt.Sprintf("%v", err)
 	errLogger.Printf("goproxy: %s\n", msg)
 	_, _ = w.Write([]byte(msg))
 }
 
 func ReturnBadRequest(w http.ResponseWriter, err error) {
-	w.WriteHeader(400)
+	w.WriteHeader(http.StatusBadRequest)
 	msg := fmt.Sprintf("%v", err)
 	errLogger.Printf("goproxy: %s\n", msg)
 	_, _ = w.Write([]byte(msg))
+}
+
+func ReturnSuccess(w http.ResponseWriter, data []byte) {
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(data)
 }
